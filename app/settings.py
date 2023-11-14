c19010ad408b8efe1fb04a16f0b9f5596d4029f9
Shairbekov Bakyt <shairbekovbakyt@icloud.com>
2023-11-14 21:59:19 +0600

    Исправлено настройка проекта secret key

--- app/settings.py
+++ app/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-+s*-zuc=3llc1x!yyt7mlh1312i!glx4cjsbk+7=8k^u#7mk16"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
