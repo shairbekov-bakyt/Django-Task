97bcfd7b4ddf3409a581d94545355755964c513f
Shairbekov Bakyt <shairbekovbakyt@icloud.com>
2023-11-14 21:04:00 +0600

    Исправлено настройка smtp аккаунта

--- app/settings.py
+++ app/settings.py
@@ -154,8 +154,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_USE_TLS = True
 EMAIL_HOST = "smtp.gmail.com"
-EMAIL_HOST_USER = "siteseogram@gmail.com"
-EMAIL_HOST_PASSWORD = "yrfe dqoo fgsi howx"
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_PASSWORD")
 EMAIL_PORT = 587
 
 
